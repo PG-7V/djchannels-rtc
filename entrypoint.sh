cca035efc390bb396903a143d351aaf8af30fd10
pg-7v <vasalenovos@gmail.com>
2023-02-24 15:08:47 +0300

    added ip:port

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 
 python manage.py migrate
 
+python manage.py loaddata dump.json
+
 #gunicorn --bind 0.0.0.0:8000 djangochannels.asgi -w 4 -k uvicorn.workers.UvicornWorker
-python manage.py runserver
+python manage.py runserver 0:8000
